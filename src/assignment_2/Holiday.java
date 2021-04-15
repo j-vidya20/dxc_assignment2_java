573b390c3c8ea99e93e808bf2277686f685511b1
J sai sree vidya <52270127+j-vidya20@users.noreply.github.com>
2021-04-16 00:19:50 +0530

    Add files via upload

---
+++ src/assignment_2/Holiday.java
@@ -0,0 +1,18 @@
+package assignment_2;
+
+public class Holiday {
+	
+		
+		private String name="New Year";
+		private int day=1;
+		private String month="jan";
+
+	
+	 public static void main(String args[])
+	 {
+		 Holiday h = new Holiday();
+		 System.out.println(h.month+" "+h.day+" is "+h.name);
+	 }
+			
+		
+	}
